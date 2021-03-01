ad800594aafd99eb74f96b3823b1a5d58cb0a904
Daniel Reiling <daniel.reiling@gmail.com>
2021-03-01 09:01:53 -0600

    Initial commit

---
+++ testing-published-properties/testing_published_propertiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  testing_published_propertiesApp.swift
+//  testing-published-properties
+//
+//  Created by Daniel Reiling on 3/1/21.
+//
+
+import SwiftUI
+
+@main
+struct testing_published_propertiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
